2a6b8214ef4d1f0219c30b1ab717d40a62685169
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-08-22 15:22:55 -0500

    comments

--- Computer.cs
+++ Computer.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace Objects_Jackson_Lydia
 {
+    // creates an internal class called computer and implements the IBootUp interface
     internal class Computer : IBootUp
     {
+        //creates property for IsOn with a get and set
         public bool IsOn { get; set; }
 
+        //creates property for Brand with a get and set
         public string Brand { get; set; }
 
+        //creates property for Type with a get and set
         public string Type { get; set; }
 
+        //parameters for brand tpe and IsOn
         public Computer(string brand, string type, bool isOn)
         {
             Brand = brand;
@@ -24,17 +29,20 @@ namespace Objects_Jackson_Lydia
 
         public void PowerOnOff()
         {
+            // prints to console if IsOn is equal to true
             IsOn = !IsOn;
             if (IsOn == true) 
             {
                 Console.WriteLine("The Computer is Booting Up.");
             }
+            // prints to console if IsOn is equal to false
             else
             {
                 Console.WriteLine("The Computer is Shutting down.");
             }
         }
 
+        // returns a description of the computer
         public override string ToString()
         {
             return $"The Computer is Made by {Brand} and is a {Type} Computer.";
